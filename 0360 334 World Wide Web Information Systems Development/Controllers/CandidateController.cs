e4b401c55077b31ecade0db46c3668f95bb47787
Jason Morand-Choquette <jasonmorand@gmail.com>
2019-07-10 09:53:34 -0400

    Add files via upload

---
+++ 0360 334 World Wide Web Information Systems Development/Controllers/CandidateController.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections.Generic;
+using CareerWebsite.Interfaces;
+using CareerWebsite.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareerWebsite.Controllers
+{
+    [Authorize(Roles = nameof(Candidate))]
+    public class CandidateController : Controller
+    {
+        private readonly ICandidateRepository _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public CandidateController(ICandidateRepository context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public ActionResult Apply(int id)
+        {           
+            _context.AddAppliedJob(User.Identity.Name, id);            
+            return RedirectToAction(nameof(Index));
+        }
+
+        public ActionResult Favourites(int id)
+        {
+            _context.AddFavouriteJob(User.Identity.Name, id);  
+            return RedirectToAction(nameof(Index));
+        }
+
+         public ActionResult RemoveAppliedJob(int id)
+        {           
+            _context.RemoveAppliedJob(id);            
+            return RedirectToAction(nameof(Index));
+        }
+
+        public ActionResult RemoveFavouriteJob(int id)
+        {
+            _context.RemoveFavouriteJob(id);  
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        // GET: Candidate
+        public ActionResult Index()
+        {           
+            var fj = _context.GetCandidateFavouriteJobs(User.Identity.Name);
+            var aj = _context.GetCandidateAppliedJobs(User.Identity.Name);
+            var model = new UserDetailsViewModel
+            {
+                AppliedJobs = aj,
+                FavouriteJobs = fj
+            };
+            return View(model);
+        }
+
+        // GET: Candidate/Details/5
+        public ActionResult Details(int? id)
+        {
+            var candidate = _context.GetCandidate(User.Identity.Name);
+            return View(candidate);
+        }
+
+        // GET: Candidate/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Candidate/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Candidate/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Candidate/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Candidate/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Candidate/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ 0360 334 World Wide Web Information Systems Development/Controllers/JobsController.cs
@@ -0,0 +1,217 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CareerWebsite.Interfaces;
+using CareerWebsite.Models;
+using Microsoft.AspNetCore.Authorization;
+using ReflectionIT.Mvc.Paging;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using CareerWebsite.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CareerWebsite.Controllers
+{
+    [Authorize(Roles = "Employer, Admins")]
+    public class JobsController : Controller
+    {
+        private readonly IJobRepository _context;
+        private readonly ApplicationDbContext _dbContext;
+        public JobsController(IJobRepository context, ApplicationDbContext dbContext)
+        {
+            _context = context;
+            _dbContext = dbContext;
+        }
+
+        
+        [AllowAnonymous]
+        // GET: Jobs
+        public IActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            List<Job> jobs = _context.Jobs;
+            List<Job> j2 = new List<Job>();
+            // different list of jobs based on user
+            if(User.IsInRole(nameof(Employer)))
+            {
+                jobs = _context.GetJobsByEmployerName(User.Identity.Name).Result;
+            }
+
+            else
+            {
+                if (searchString != null) page = 1;            
+                else searchString = currentFilter;
+            
+                ViewBag.CurrentFilter = searchString;
+            
+
+                if (!string.IsNullOrEmpty(searchString))
+                {                
+                    j2.AddRange(_context.GetJobsByTitle(searchString).Result);
+                    j2.AddRange(_context.GetJobsByCity(searchString).Result);
+                    j2.AddRange(_context.GetJobsByEmployerName(searchString).Result);
+                }            
+
+                if(j2.Any())
+                {
+                    jobs = new List<Job>();
+                    jobs.AddRange(j2);
+                }          
+            }            
+
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            ViewBag.PageNumber = pageNumber;           
+            var model = PagingList.Create(jobs, pageSize, pageNumber);
+
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        // GET: Jobs/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var job = await _context.GetJobById(id);            
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            return View(job);
+        }
+
+        //GET: Jobs/Create
+        public IActionResult Create()
+        {
+            ViewData["JobTypeId"] = new SelectList(_dbContext.JobTypes, "Id", "Name");
+            ViewData["MinEducationId"] = new SelectList(_dbContext.Education, "Id", "Name");
+            ViewData["ProvinceId"] = new SelectList(_dbContext.Provinces, "Id", "Name");
+            ViewData["CategoryId"] = new SelectList(_dbContext.Categories, "Id", "Name");
+            return View(new Job());
+        }
+
+        // POST: Jobs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(/*[Bind("Title,Salary,NoOfVacancies,JobDescription,ocation_City,JobLocation_ProvinveId,JobLocation_PostalCode,Closing_Date,Start_Date,MinEducationId,JobTypeId")]*/ Job job) //, Location.ProvinceId, Location.City, Location.PostalCode
+        {            
+            var employer = _context.GetEmployer(User.Identity.Name);
+            job.Employer = employer;            
+            job.Date_Posted = DateTime.Today;
+
+
+            if (ModelState.IsValid)
+            {
+                _dbContext.Add(job);
+                await _dbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["JobTypeId"] = new SelectList(_dbContext.JobTypes, "Id", "Name", job.JobTypeId);
+            ViewData["MinEducationId"] = new SelectList(_dbContext.Education, "Id", "Name", job.MinEducationId);
+            ViewData["ProvinceId"] = new SelectList(_dbContext.Provinces, "Id", "Name", job.JobLocation.ProvinceId);
+            ViewData["CategoryId"] = new SelectList(_dbContext.Categories, "Id", "Name", job.CategoryId);
+            return View(job);
+        }
+
+        // GET: Jobs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var job = await _dbContext.Jobs.SingleOrDefaultAsync(m => m.Id == id);
+            if (job == null)
+            {
+                return NotFound();
+            }
+            ViewData["JobTypeId"] = new SelectList(_dbContext.JobTypes, "Id", "Name", job.JobTypeId);
+            ViewData["MinEducationId"] = new SelectList(_dbContext.Education, "Id", "Name", job.MinEducationId);
+            ViewData["ProvinceId"] = new SelectList(_dbContext.Provinces, "Id", "Name", job.JobLocation.ProvinceId);
+            ViewData["CategoryId"] = new SelectList(_dbContext.Categories, "Id", "Name", job.CategoryId);
+            return View(job);
+        }
+
+        // POST: Jobs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, /*[Bind("Id,Title,Salary,NoOfVacancies,JobDescription,Closing_Date,Start_Date,MinEducationId,JobTypeId")]*/ Job job)
+        {
+            if (id != job.Id)
+            {
+                return NotFound();
+            }
+
+            var employer = _context.GetEmployer(User.Identity.Name);
+            job.Employer = employer;            
+            job.Date_Posted = DateTime.Today;
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _dbContext.Update(job);
+                    await _dbContext.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!JobExists(job.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["JobTypeId"] = new SelectList(_dbContext.JobTypes, "Id", "Name", job.JobTypeId);
+            ViewData["MinEducationId"] = new SelectList(_dbContext.Education, "Id", "Name", job.MinEducationId);
+            ViewData["ProvinceId"] = new SelectList(_dbContext.Provinces, "Id", "Name", job.JobLocation.ProvinceId);
+            ViewData["CategoryId"] = new SelectList(_dbContext.Categories, "Id", "Name", job.CategoryId);
+            return View(job);
+        }
+
+        // GET: Jobs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var job = await _dbContext.Jobs
+                .Include(j => j.JobType)
+                .Include(j => j.MinEducation)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            return View(job);
+        }
+
+        // POST: Jobs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var job = await _dbContext.Jobs.SingleOrDefaultAsync(m => m.Id == id);
+            _dbContext.Jobs.Remove(job);
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool JobExists(int id)
+        {
+            return _context.Jobs.Any(e => e.Id == id);
+        }
+    }
+}
