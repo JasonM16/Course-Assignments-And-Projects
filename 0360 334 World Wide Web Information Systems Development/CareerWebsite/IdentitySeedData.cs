0b7f8691b31a93c2c37c3122236104150710e725
Jason Morand-Choquette <jasonmorand@gmail.com>
2019-07-10 09:57:27 -0400

    Add files via upload

---
+++ 0360 334 World Wide Web Information Systems Development/CareerWebsite/IdentitySeedData.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using CareerWebsite.Models;
+
+namespace CareerWebsite
+{
+    public static class IdentitySeedData
+    {
+        public static async Task CreateAdminAccount(IServiceProvider serviceProvider,
+            IConfiguration configuration)
+        {
+            IServiceScopeFactory scopeFactory = serviceProvider.GetRequiredService<IServiceScopeFactory>();
+
+            using (IServiceScope scope = scopeFactory.CreateScope())
+            {
+
+                UserManager<ApplicationUser> userManager =
+                    scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+                RoleManager<IdentityRole> roleManager =
+                    scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                string username = configuration["AdminUser:Name"];
+                string email = configuration["AdminUser:Email"];
+                string password = configuration["AdminUser:Password"];
+                string role = configuration["AdminUser:Role"];
+
+                if (await userManager.FindByNameAsync(username) == null)
+                {
+                    if (await roleManager.FindByNameAsync(role) == null)
+                    {
+                        await roleManager.CreateAsync(new IdentityRole(role));
+                    }
+
+                    ApplicationUser user = new ApplicationUser
+                    {
+                        UserName = username,
+                        Email = email
+                    };
+
+                    IdentityResult result = await userManager
+                        .CreateAsync(user, password);
+                    if (result.Succeeded)
+                    {
+                        await userManager.AddToRoleAsync(user, role);
+                    }
+                }
+            }
+        }
+    }
+}
